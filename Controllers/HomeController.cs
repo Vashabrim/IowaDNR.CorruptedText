9933e77ff779ad952b406197089c28710bc498ec
Doug Stubbs <Vashabrim@users.noreply.github.com>
2020-09-09 20:39:49 -0500

    code basically working, not pretty and some questions about formatting, but it is functional.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using IowaDNR.CorruptedText.Models;
+using System.Text.RegularExpressions;
 
 namespace IowaDNR.CorruptedText.Controllers
 {
@@ -23,6 +24,39 @@ namespace IowaDNR.CorruptedText.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> SearchText()
+        {
+            var txtToSearch = HttpContext.Request.Form["TextToSearch"];
+            var txtToFind = HttpContext.Request.Form["SearchCombo"];
+            var count = await SearchTheText(txtToSearch, txtToFind);
+            return Content("Hello, the combination " + txtToFind + " was found " + count.ResultsCount.ToString() + " times");
+        }
+
+        public async Task<ResultsViewModel> SearchTheText(string results, string textToFind)
+        {
+            var cleanResults = CleanText(results, textToFind);
+            ResultsViewModel model = new ResultsViewModel()
+            {
+                SearchText = results,
+                TextToFind = textToFind,
+            };
+            model.ResultsCount = cleanResults;
+            return model;
+        }
+
+        private int CleanText(string textToClean, string txtToFind)
+        {
+            string pattern = "[^GCAT]";
+            //Are lowercase 'gcat' characters valid?
+            var count = 0;
+            var cleanText = Regex.Replace(textToClean, pattern, string.Empty);
+            foreach (Match match in Regex.Matches(cleanText, txtToFind.ToUpper()))
+            {
+                count++;
+            };
+            return count;
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ Models/ResultsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IowaDNR.CorruptedText.Models
+{
+    public class ResultsViewModel
+    {
+        public string SearchText { get; set; }
+        public string TextToFind { get; set; }
+        public int ResultsCount { get; set; }
+    }
+}
